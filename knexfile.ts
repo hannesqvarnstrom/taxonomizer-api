0e285d4d680c08b98158de5c4b6f839fb58a6e02
Hannes Qvarnström <hannesqvarnstrom@gmail.com>
2022-05-17 08:58:43 +0200

    add ssl handling?

--- knexfile.ts
+++ knexfile.ts
@@ -2,9 +2,17 @@ import type { Knex } from "knex";
 import * as dotenv from 'dotenv'
 dotenv.config()
 // Update with your config settings.
+// This is a fix to force knex to connect using ssl in production
+// This is a requirement by Heroku.
+// Read more at: https://github.com/tgriesser/knex/issues/852#issuecomment-229502678
+import pg from 'pg'
+if (['production', 'staging'].includes(process.env.NODE_ENV)) {
+  pg.defaults.ssl = { rejectUnauthorized: false }
+}
 const isProduction = process.env.NODE_ENV === 'production'
 const dbURL = process.env.DATABASE_URL
-const connection = isProduction ? `${dbURL}?ssl=true` : dbURL
+const connection = dbURL
+
 const config: { [key: string]: Knex.Config } = {
     development: {
         client: "postgresql",
