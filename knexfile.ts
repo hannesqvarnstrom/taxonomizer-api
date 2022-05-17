bb4a514c3f76925dd57cdff0356116d2fbc533c6
Hannes Qvarnström <hannesqvarnstrom@gmail.com>
2022-05-17 08:27:56 +0200

    migrations for prod

--- knexfile.ts
+++ knexfile.ts
@@ -2,13 +2,13 @@ import type { Knex } from "knex";
 import * as dotenv from 'dotenv'
 dotenv.config()
 // Update with your config settings.
-console.log(process.env)
+
 const config: { [key: string]: Knex.Config } = {
     development: {
         client: "postgresql",
         connection: process.env.DATABASE_URL,
         migrations: {
-            tableName: "migrations",
+            tableName: "dist/migrations",
         },
     },
 
